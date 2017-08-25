25bc6578149de2d15fb8d95b3e897723b0a4ba35
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-25 20:21:31 +0800

    stateless Components

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'passenger-count',
+    template: `
+       Count Compoent
+    `
+})
+export class PassengerCountComponent {
+
+}
---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'passenger-detail',
+    template: `
+       detail Compoent
+    `
+})
+export class PassengerDetailComponent {
+
+}
