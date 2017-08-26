5c51051d172e8043a11b2fb74285aa585836c89e
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-26 13:53:18 +0800

    <select> option rendering, and ngValue

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,6 +1,7 @@
-import { type } from 'os';
-import { Passenger } from './../../models/passenger.interface';
 import { Component, Input } from '@angular/core';
+
+import { Baggage } from '../../models/baggage.interface';
+import { Passenger } from './../../models/passenger.interface';
 
 @Component({
     selector: 'passenger-form',
@@ -62,6 +63,30 @@ import { Component, Input } from '@angular/core';
                 [ngModel]="detail?.checkInDate">
           </div>
 
+          <div>
+             Luggage:
+             <select name="baggage"
+               [ngModel]="detail?.baggage">
+               <option
+                   *ngFor="let item of baggage"
+                   [value]="item.key"
+                   [selected]="item.key === detail?.baggage"
+                   >
+                   {{ item.value }}
+               </option>
+             </select>
+
+             <select name="baggage"
+                  [ngModel]="detail?.baggage">
+                <option
+                    *ngFor="let item of baggage"
+                    [ngValue]="item.key"
+                    >
+                    {{ item.value }}
+                </option>
+             </select>
+          </div>
+
 
           {{ form.value | json }}
        </form>
@@ -70,6 +95,25 @@ import { Component, Input } from '@angular/core';
 export class PassengerFormComponent {
     @Input()
     detail: Passenger;
+
+    baggage: Baggage[] = [{
+        key: 'none',
+        value: 'No baggage'
+    },
+    {
+        key: 'hand-only',
+        value: 'Hand baggage'
+    },
+    {
+        key: 'hold-only',
+        value: 'Hold baggage'
+    },
+    {
+        key: 'hand-hold',
+        value: 'Hand and Hold baggage'
+    }
+
+];
 
     toggleCheckIn(checkedIn: Boolean){
         this.detail.checkInDate = Date.now();
