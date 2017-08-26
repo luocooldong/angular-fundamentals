8d153d314de5d546607ca4d7f25a64077f359b34
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-26 08:43:12 +0800

    Emitting changes with @Output and EventEmitter

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -12,7 +12,9 @@ import { Passenger } from '../../models/passenger.interface';
       </passenger-count>
       <passenger-detail
         *ngFor="let passenger of passengers;"
-        [detail]="passenger" >
+        [detail]="passenger"
+        (edit)="handleEdit($event)"
+        (remove)="handleRemove($event)" >
       </passenger-detail>
     </div>
   `
@@ -59,6 +61,14 @@ export class PassengerDashboardComponent implements OnInit {
       checkInDate: null,
       children: null
     }];
+  }
+
+  handleEdit(event){
+    console.log(event);
+  }
+
+  handleRemove(event){
+    console.log(event);
   }
 
 }
